7ea27d2b0052d019470935771a5f3007e1a2a33d
liav656levi-pixel <liav656levi@gmail.com>
2026-02-23 12:39:18 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@theme/tailwindcss'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  base: '/test-2/',
 })
